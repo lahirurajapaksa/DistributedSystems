1d2ae1218203c0302544065947d35379b9d9f4b9
lahirurajapaksa <lahiru9954@gmail.com>
2020-03-05 22:39:29 +0000

    Connected

--- frontendserver.py
+++ frontendserver.py
@@ -1,17 +1,36 @@
 import Pyro4
 
 #acts as a bridge between the client and the backend severs
+def connecttobackendserver():
+	ipaddress = "192.168.1.3"
+
+	FoodMenu = Pyro4.core.Proxy('PYRO:FOOD@'+ ipaddress + ':9090')
+
+	return FoodMenu
 
 #retrieve the menus from the backend
-def getfoodmenusbackend():
-	uri = input("what is the uri?").strip()
-	FoodMenu = Pyro4.Proxy(uri)      # get a Pyro proxy to the FoodMenu object
+def getfoodmenusbackend(FoodMenu):
 
-	print("response is ",FoodMenu.foodmenureturn())
-	#store the returned dictionary in FoodDict
 	FoodDict = FoodMenu.foodmenureturn()
 
+	print(FoodDict)
 	return FoodDict
 
-FoodDict = getfoodmenusbackend()
+classreturn = connecttobackendserver()
 
+FoodDict = getfoodmenusbackend(classreturn)
+
+def connecttoclient():
+
+	@Pyro4.expose
+	class FoodMenufrontend(object):
+		def foodmenureturn(self):
+			#send the food dict to the front end server
+			return FoodDict
+
+
+	Pyro4.Daemon.serveSimple({
+	    FoodMenufrontend: 'FOOD',
+	}, host="192.168.1.3", port=9091, ns=False, verbose=True)
+
+connecttoclient()
