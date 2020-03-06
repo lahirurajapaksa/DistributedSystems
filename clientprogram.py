b17372a6188ab5bb2e186602bd40a7a7c743477a
lahirurajapaksa <lahiru9954@gmail.com>
2020-03-06 00:33:24 +0000

    added error checkign for user input

--- clientprogram.py
+++ clientprogram.py
@@ -1,13 +1,45 @@
 import Pyro4
-
+import sys
 #display questions to the user to get the user's basic info to facilitate delivery
 def getuserinfo():
+	valid = False
+	while valid==False:
+		firstname = input("First Name: ").strip()
+		if firstname.isalpha()==False:
+			print("Do not use any non-alphabetic characters")
+		else:
+			valid = True
 
-	name = input("Full Name: ").strip()
+	valid = False
+	while valid == False:
+		lastname = input("Last Name: ").strip()
+		if lastname.isalpha()==False:
+			print("Do not use any non-alphabetic characters")
+		else:
+			valid = True
+	
 	deliveryAddress = input("Delivery address: ").strip()
+
 	postcode = input("Postcode: ").strip()
-	restaurantname = input("Which restaurant would you like to place your order at?")
-	ordernumbers = input("Enter the 'numbers' corresponding to your order. If you would like more than one order of the same portion, please repeat this number the desired amount. Example:'1,2,2,4' :")
+
+	valid = False
+	while valid==False:
+		restaurantname = input("Which restaurant would you like to place your order at?").strip()
+		if restaurantname.isalpha()==False:
+			print("Do not use any non-alphabetic characters")
+		else:
+			valid = True
+
+	valid = False
+	while valid == False:
+		ordernumbers = input("Enter the 'numbers' corresponding to your order. If you would like more than one order of the same portion, please repeat this number the desired amount. Example:'1,2,2,4' :").strip()
+		for i in range(0,len(ordernumbers),2):
+		    if ordernumbers[i].isdigit()==False:
+		        print("only use numerals")
+		    else:
+		    	valid = True
+	        
+
 def connecttofrontend():
 	ipaddress = "192.168.1.3"
 
