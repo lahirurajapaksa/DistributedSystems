f271ab9c775bce73ca381f05e14bcacdc23cadc0
lahirurajapaksa <lahiru9954@gmail.com>
2020-03-11 16:13:40 +0000

    Backing up before using postcode validation

--- backendserverbackup.py
+++ backendserverbackup.py
@@ -227,16 +227,16 @@ def storemenus():
 	foodDict={}
 	#define menu for Chinese Dragon
 	chinesedragonmenu =['1. Hot Butter Cuttlefish','2. Sweet and Sour Fish','3. Garlic Prawns','4. Cajun Chicken', '5. Roasted Duck']
-	foodDict['Chinese Dragon'] = chinesedragonmenu
+	foodDict['chinese dragon'] = chinesedragonmenu
 	#define menu for Mc Donalds
 	MCmenu = ['1. Cheesburger','2. Filet -o- fish','3. Mc Spicy','4. Traditional Burger','5. Mc Special']
-	foodDict['Mc Donalds']=MCmenu
+	foodDict['mc donalds']=MCmenu
 	#define menu for Pizza Hut
 	pizzamenu = ['1. Margerita','2. Chicken Supreme','3. Chicken Hawaiian','4. Pepperoni','5. Jalopeno and Pork']
-	foodDict['Pizza Hut'] = pizzamenu
+	foodDict['pizza hut'] = pizzamenu
 	#define menu for exotic dishes
 	exoticmenu = ['1. Chicken Feet','2. Snail', '3. Bear Droppings', '4. Dog Ice Cream', '5. Shark Uterus']
-	foodDict['Exotic Dishes'] = exoticmenu
+	foodDict['exotic dishes'] = exoticmenu
 
 	return foodDict
 
